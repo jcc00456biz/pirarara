b463095f0943eadf59e803107e4c21292f0767b5
jcc00456 <jcc00456biz@gmail.com>
2025-02-25 22:56:56 +0900

    Python+Qtアプリ開発日記#8

--- src/pirarara/gui/gmain.py
+++ src/pirarara/gui/gmain.py
@@ -2,23 +2,61 @@
 # -*- coding: utf-8 -*-
 
 
-from PySide6.QtCore import QMetaObject, QRect
-from PySide6.QtWidgets import QMainWindow, QMenuBar, QStatusBar, QWidget
+from PySide6.QtCore import (
+    QMetaObject,
+    QRect,
+    Qt,
+)
+
+from PySide6.QtWidgets import (
+    QComboBox,
+    QGraphicsView,
+    QMainWindow,
+    QMenuBar,
+    QPlainTextEdit,
+    QSplitter,
+    QStatusBar,
+    QTableWidget,
+    QTreeWidget,
+    QVBoxLayout,
+    QWidget,
+)
 
 
 class MWindow(QMainWindow):
-    def setupUi(self, MainWindow):
+    def __init__(self):
+        super().__init__()
 
-        MainWindow.resize(415, 263)
+        self.resize(784, 564)
+        self.centralwidget = QWidget(self)
 
-        self.centralwidget = QWidget(MainWindow)
-        MainWindow.setCentralWidget(self.centralwidget)
+        self.verticalLayout = QVBoxLayout(self.centralwidget)
 
-        self.menubar = QMenuBar(MainWindow)
-        self.menubar.setGeometry(QRect(0, 0, 415, 33))
-        MainWindow.setMenuBar(self.menubar)
+        self.comboBox = QComboBox(self.centralwidget)
 
-        self.statusbar = QStatusBar(MainWindow)
-        MainWindow.setStatusBar(self.statusbar)
+        self.verticalLayout.addWidget(self.comboBox)
 
-        QMetaObject.connectSlotsByName(MainWindow)
+        self.splitter_2 = QSplitter(self.centralwidget)
+        self.splitter_2.setOrientation(Qt.Orientation.Horizontal)
+        self.treeWidget = QTreeWidget(self.splitter_2)
+        self.splitter_2.addWidget(self.treeWidget)
+        self.tableWidget = QTableWidget(self.splitter_2)
+        self.splitter_2.addWidget(self.tableWidget)
+        self.splitter = QSplitter(self.splitter_2)
+        self.splitter.setOrientation(Qt.Orientation.Vertical)
+        self.graphicsView = QGraphicsView(self.splitter)
+        self.splitter.addWidget(self.graphicsView)
+        self.plainTextEdit = QPlainTextEdit(self.splitter)
+        self.splitter.addWidget(self.plainTextEdit)
+        self.splitter_2.addWidget(self.splitter)
+
+        self.verticalLayout.addWidget(self.splitter_2)
+
+        self.setCentralWidget(self.centralwidget)
+        self.menubar = QMenuBar(self)
+        self.menubar.setGeometry(QRect(0, 0, 784, 33))
+        self.setMenuBar(self.menubar)
+        self.statusbar = QStatusBar(self)
+        self.setStatusBar(self.statusbar)
+
+        QMetaObject.connectSlotsByName(self)
--- ui_design/ui/main_window_ui.py
+++ ui_design/ui/main_window_ui.py
@@ -15,20 +15,54 @@ from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
     QFont, QFontDatabase, QGradient, QIcon,
     QImage, QKeySequence, QLinearGradient, QPainter,
     QPalette, QPixmap, QRadialGradient, QTransform)
-from PySide6.QtWidgets import (QApplication, QMainWindow, QMenuBar, QSizePolicy,
-    QStatusBar, QWidget)
+from PySide6.QtWidgets import (QApplication, QComboBox, QGraphicsView, QHeaderView,
+    QMainWindow, QMenuBar, QPlainTextEdit, QSizePolicy,
+    QSplitter, QStatusBar, QTableWidget, QTableWidgetItem,
+    QTreeWidget, QTreeWidgetItem, QVBoxLayout, QWidget)
 
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         if not MainWindow.objectName():
             MainWindow.setObjectName(u"MainWindow")
-        MainWindow.resize(415, 263)
+        MainWindow.resize(784, 564)
         self.centralwidget = QWidget(MainWindow)
         self.centralwidget.setObjectName(u"centralwidget")
+        self.verticalLayout = QVBoxLayout(self.centralwidget)
+        self.verticalLayout.setObjectName(u"verticalLayout")
+        self.comboBox = QComboBox(self.centralwidget)
+        self.comboBox.setObjectName(u"comboBox")
+
+        self.verticalLayout.addWidget(self.comboBox)
+
+        self.splitter_2 = QSplitter(self.centralwidget)
+        self.splitter_2.setObjectName(u"splitter_2")
+        self.splitter_2.setOrientation(Qt.Orientation.Horizontal)
+        self.treeWidget = QTreeWidget(self.splitter_2)
+        __qtreewidgetitem = QTreeWidgetItem()
+        __qtreewidgetitem.setText(0, u"1");
+        self.treeWidget.setHeaderItem(__qtreewidgetitem)
+        self.treeWidget.setObjectName(u"treeWidget")
+        self.splitter_2.addWidget(self.treeWidget)
+        self.tableWidget = QTableWidget(self.splitter_2)
+        self.tableWidget.setObjectName(u"tableWidget")
+        self.splitter_2.addWidget(self.tableWidget)
+        self.splitter = QSplitter(self.splitter_2)
+        self.splitter.setObjectName(u"splitter")
+        self.splitter.setOrientation(Qt.Orientation.Vertical)
+        self.graphicsView = QGraphicsView(self.splitter)
+        self.graphicsView.setObjectName(u"graphicsView")
+        self.splitter.addWidget(self.graphicsView)
+        self.plainTextEdit = QPlainTextEdit(self.splitter)
+        self.plainTextEdit.setObjectName(u"plainTextEdit")
+        self.splitter.addWidget(self.plainTextEdit)
+        self.splitter_2.addWidget(self.splitter)
+
+        self.verticalLayout.addWidget(self.splitter_2)
+
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QMenuBar(MainWindow)
         self.menubar.setObjectName(u"menubar")
-        self.menubar.setGeometry(QRect(0, 0, 415, 33))
+        self.menubar.setGeometry(QRect(0, 0, 784, 33))
         MainWindow.setMenuBar(self.menubar)
         self.statusbar = QStatusBar(MainWindow)
         self.statusbar.setObjectName(u"statusbar")
