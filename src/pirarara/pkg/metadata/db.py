31f320dde637b0f535b1c8d9e5ab9e9bf1273a3f
jcc00456 <jcc00456biz@gmail.com>
2025-03-02 01:17:21 +0900

    Python+Qtアプリ開発日記#14

--- src/pirarara/pkg/metadata/db.py
+++ src/pirarara/pkg/metadata/db.py
@@ -45,7 +45,6 @@ class MetaDataDB:
         # DBファイル
         self.db_file_path = db_file_path
         # テーブル名
-        # self.table_name = self.__class__.__name__
         self.table_name = "MetaDataTbl"
 
         # テーブルカラム定義
@@ -143,3 +142,198 @@ class MetaDataDB:
             cursor.executescript(sql)
 
         gc.collect()
+
+    def insert(self, columns: list, values: list) -> int | None:
+        """
+        指定されたカラムと値を使用してデータベースに新しいレコードを挿入します。
+
+        Args:
+            columns (list): 挿入するカラムのリスト。
+            values (list): 挿入する値のリスト。各値の型は対応するカラムの型と一致する必要があります。
+
+        Raises:
+            TypeError: `columns`または`values`がリストでない場合、または各値の型がカラムの型と一致しない場合。
+            ValueError: `columns`と`values`の長さが一致しない場合、または`columns`に無効な値が
+            含まれている場合。
+
+        Returns:
+            int | None: 挿入された行のID。挿入が失敗した場合は`None`。
+        """
+        if not isinstance(columns, list):
+            raise TypeError("columns must be of type list")
+        if not isinstance(values, list):
+            raise TypeError("values must be of type list")
+        if len(columns) != len(values):
+            raise ValueError("columns and number of values do not match")
+        if not all(item in self.table_columns for item in columns):
+            raise ValueError("columns contains invalid values")
+        for index, c in enumerate(columns):
+            t = self.table_columns[c]
+            if "INTEGER" in t:
+                if not isinstance(values[index], int):
+                    raise TypeError("values must be of type int")
+            if "TEXT" in t:
+                if not isinstance(values[index], str):
+                    raise TypeError("values must be of type str")
+
+        sql_values = ", ".join(["?" for _ in values])
+        sql = (
+            f"INSERT INTO {self.table_name} "
+            + f"({', '.join(columns)}) VALUES ({sql_values});"
+        )
+
+        with sqlite3.connect(self.db_file_path, isolation_level=None) as conn:
+            cursor = conn.cursor()
+            cursor.execute(sql, tuple(values))
+            return cursor.lastrowid
+
+    def update(self, id: int, columns: list, values: list) -> None:
+        """
+        指定されたIDを持つレコードを更新します。
+
+        Args:
+            id (int): 更新する行のID。
+            columns (list): 更新するカラムのリスト。
+            values (list): 更新する値のリスト。各値の型は対応するカラムの型と一致する必要があります。
+
+        Raises:
+            TypeError: `id` が整数型でない場合、`columns` または `values` が
+            リストでない場合、または各値の型がカラムの型と一致しない場合。
+            ValueError: `columns` と `values` の長さが一致しない場合、
+            または `columns` に無効な値が含まれている場合。
+
+        Returns:
+            None
+        """
+        if not isinstance(id, int):
+            raise TypeError("id must be of type int")
+        if not isinstance(columns, list):
+            raise TypeError("columns must be of type list")
+        if not isinstance(values, list):
+            raise TypeError("values must be of type list")
+        if len(columns) != len(values):
+            raise ValueError("columns and number of values do not match")
+        if not all(item in self.table_columns for item in columns):
+            raise ValueError("columns contains invalid values")
+        for index, c in enumerate(columns):
+            t = self.table_columns[c]
+            if "INTEGER" in t:
+                if not isinstance(values[index], int):
+                    raise TypeError("values must be of type int")
+            if "TEXT" in t:
+                if not isinstance(values[index], str):
+                    raise TypeError("values must be of type str")
+
+        wk_columns = [f"{c}=?" for c in columns]
+        wk_values = values + [id]
+
+        sql = (
+            f"UPDATE {self.table_name} "
+            + f"SET {','.join(wk_columns)} "
+            + "WHERE id=?;"
+        )
+        with sqlite3.connect(self.db_file_path, isolation_level=None) as conn:
+            cursor = conn.cursor()
+            cursor.execute(sql, tuple(wk_values))
+
+    def delete(self, id: int) -> None:
+        """
+        指定されたIDに基づいてテーブルからレコードを削除するメソッド。
+        削除後テーブルにデータが存在しなくなった場合に自動更新値をリセットする
+
+        Args:
+            db_id (int): 対象の一意の識別子。
+        """
+        with sqlite3.connect(self.db_file_path) as conn:
+            cursor = conn.cursor()
+            cursor.execute(
+                f"SELECT protection FROM {self.table_name} WHERE id=?;", (id,)
+            )
+            result = cursor.fetchone()
+        if result and result[0] == 1:
+            return
+
+        with sqlite3.connect(self.db_file_path) as conn:
+            cursor = conn.cursor()
+            cursor.execute(f"DELETE FROM {self.table_name} WHERE id=?;", (id,))
+
+        with sqlite3.connect(self.db_file_path) as conn:
+            cursor = conn.cursor()
+            cursor.execute(f"SELECT COUNT(*) FROM {self.table_name}")
+            row_count = cursor.fetchone()[0]
+
+        if row_count == 0:
+            with sqlite3.connect(self.db_file_path) as conn:
+                cursor = conn.cursor()
+                cursor.execute(
+                    "DELETE FROM sqlite_sequence WHERE name=?;",
+                    (f"{self.table_name}",),
+                )
+
+    def get_data(self, id: int) -> dict | None:
+        """
+        指定されたIDに基づいてテーブルからレコードを取得するメソッド。
+
+        Args:
+            db_id (int): データを取得する対象の一意の識別子。
+
+        Returns:
+            dict | None: 取得したデータを格納した辞書。データが存在しない場合はNone。
+
+        このメソッドは、指定されたIDに基づいてデータベース内の特定のテーブルからデータを取得します。
+        まず、テーブルの構造情報を取得し、その後、IDに基づいてデータを選択します。
+        取得したデータが存在しない場合はNoneを返します。存在する場合は、テーブルの列名をキーとして
+        データを辞書形式で返します。
+        """
+        with sqlite3.connect(self.db_file_path) as conn:
+            cursor = conn.cursor()
+            cursor.execute(f"PRAGMA TABLE_INFO ({self.table_name});")
+            table_columns = cursor.fetchall()
+            cursor.execute(
+                f"SELECT * FROM {self.table_name} WHERE id=?;", (id,)
+            )
+            data = cursor.fetchone()
+
+        if data is None:
+            return None
+
+        ret_data = {}
+        for index, col in enumerate(table_columns):
+            ret_data[col[1]] = data[index]
+
+        return ret_data
+
+    def get_all_data(self) -> list | None:
+        """
+        データベースからすべてのデータを取得し、フォーマットされたリストを返します。
+
+        このメソッドは、指定されたデータベースファイルとテーブルからすべてのデータを取得し、
+        各レコードを辞書としてフォーマットし、それらの辞書をリストとして返します。
+
+        Returns:
+            list | None: データベースから取得したデータのリスト。
+            データが存在しない場合はNoneを返します。
+
+        Raises:
+            sqlite3.Error: データベース操作中にエラーが発生した場合。
+        """
+        with sqlite3.connect(self.db_file_path) as conn:
+            cursor = conn.cursor()
+            cursor.execute(f"PRAGMA TABLE_INFO ({self.table_name});")
+            table_columns = cursor.fetchall()
+            cursor.execute(f"SELECT * FROM {self.table_name} ORDER BY id ASC;")
+            data = cursor.fetchall()
+
+        if data is None:
+            return None
+
+        ret_data = []
+        for d in data:
+            roe_data = {}
+            for index, col in enumerate(table_columns):
+                if isinstance(d[index], str):
+                    roe_data[col[1]] = d[index]
+                if isinstance(d[index], int):
+                    roe_data[col[1]] = str(d[index])
+            ret_data.append(roe_data)
+        return ret_data
